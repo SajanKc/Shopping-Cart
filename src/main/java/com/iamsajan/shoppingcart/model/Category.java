5c4efb70cef40ab11e12c3d7b3a01150c061659f
sajan96kc <sajan1996kc@gmail.com>
2024-09-02 16:30:16 +0545

    fix: json tree issue

--- src/main/java/com/iamsajan/shoppingcart/model/Category.java
+++ src/main/java/com/iamsajan/shoppingcart/model/Category.java
@@ -1,5 +1,6 @@
 package com.iamsajan.shoppingcart.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ public class Category {
     private Long id;
     @NonNull
     private String name;
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 
